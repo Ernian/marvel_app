deab6a38a76e4fa0e84a39b158ca3da12c04943c
light <estarosa@list.ru>
2022-07-11 21:12:00 +0300

    create 404 page

---
+++ src/components/pages/404.js
@@ -0,0 +1,12 @@
+import ErrorBlock from "../errorBlock/ErrorBlock"
+
+const Page404 = () => {
+    return (
+        <>
+            <h2>Page dosn`t exist...</h2>
+            <ErrorBlock />
+        </>
+    )
+}
+
+export default Page404
