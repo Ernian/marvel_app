dd321c8eb82ff670a826f48ac848704ee6d37e5f
light <estarosa@list.ru>
2022-07-11 16:37:44 +0300

    react-router-dom 6.3.0

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -12,19 +12,27 @@ const AppHeader = () => {
             <nav className="app__menu">
                 <ul>
                     <li>
-                        <NavLink exact activeStyle={{ 'color': '#9f0013' }} to="/">
+                        <NavLink
+                            end
+                            to="/"
+                            style={({ isActive }) => ({ color: isActive ? '#9f0013' : null })}
+                        >
                             Characters
                         </NavLink>
                     </li>
                     /
                     <li>
-                        <NavLink exact activeStyle={{ 'color': '#9f0013' }} to="/comics">
+                        <NavLink
+                            end
+                            to="/comics"
+                            style={({ isActive }) => ({ color: isActive ? '#9f0013' : null })}
+                        >
                             Comics
                         </NavLink>
                     </li>
                 </ul>
             </nav>
-        </header>
+        </header >
     )
 }
 
